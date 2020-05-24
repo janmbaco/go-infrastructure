913ddfd727d9cb9c8d0879ce4d98b183cf51dd8d
janmbaco <janmbaco@gmail.com>
2020-05-24 09:53:56 +0200

    eventmanager

---
+++ disk/fdlimit/fdlimit_unix.go
@@ -0,0 +1,13 @@
+// +build linux netbsd openbsd solaris
+
+package fdlimit
+
+import "syscall"
+
+func Get() (int, error) {
+	var limit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
+		return 0, err
+	}
+	return int(limit.Cur), nil
+}
---
+++ disk/fdlimit/fdlimit_windows.go
@@ -0,0 +1,5 @@
+package fdlimit
+
+func Get() (int, error) {
+	return 16384, nil
+}
