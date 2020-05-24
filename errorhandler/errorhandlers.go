0c5670c7939506994054a8c40dcbc365913a49f4
janmbaco <janmbaco@gmail.com>
2020-05-24 11:06:36 +0200

    errorhandlers

--- errorhandler/errorhandlers.go
+++ errorhandler/errorhandlers.go
@@ -2,44 +2,50 @@ package errorhandler
 
 import (
 	"errors"
+
 	"github.com/janmbaco/go-infrastructure/logs"
 )
 
-func TryError(callBack func(), errorFunc func(error)) {
-	func() {
-		defer func() {
-			if re := recover(); re != nil {
-				text := "unexpected error"
-				switch re.(type) {
-				case string:
-					text = re.(string)
-				case error:
-					text = re.(error).Error()
-				}
-				errorFunc(errors.New(text))
-			}
-		}()
-		callBack()
-	}()
+func onErrorContinue(callBack func()) {
+	defer deferTryError(true, nil, nil)
+	callBack()
+}
+
+func TryCatchError(callBack func(), errorFunc func(error)) {
+	defer deferTryError(false, errorFunc, nil)
+	callBack()
 }
 
 func TryFinally(callBack func(), finallyFunc func()) {
-	func() {
-		defer func() {
-			finallyFunc()
-			if re := recover(); re != nil {
-				text := "unexpected error"
-				switch re.(type) {
-				case string:
-					text = re.(string)
-				case error:
-					text = re.(error).Error()
-				}
-				panic(errors.New(text))
-			}
-		}()
-		callBack()
-	}()
+	defer deferTryError(false, nil, finallyFunc)
+	callBack()
+}
+
+func TryCatchErrorAndFinally(callBack func(), errorFunc func(error), finallyFunc func()) {
+	defer deferTryError(false, errorFunc, finallyFunc)
+	callBack()
+}
+
+func deferTryError(shouldContinue bool, errorFunc func(error), finallyFunc func()) {
+	if finallyFunc != nil {
+		finallyFunc()
+	}
+	if re := recover(); re != nil {
+		text := "unexpected error"
+		switch re.(type) {
+		case string:
+			text = re.(string)
+		case error:
+			text = re.(error).Error()
+		}
+		logs.Log.Error(text)
+		if errorFunc != nil {
+			errorFunc(errors.New(text))
+		} else if !shouldContinue {
+			panic(errors.New(text))
+		}
+
+	}
 }
 
 func TryPanic(err error) {
