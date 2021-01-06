8d1f4301ae7797bb5dd56020799aa9506757bb66
janmbaco <janmbaco@gmail.com>
2021-01-06 13:42:15 +0100

    Fix - set safe pointer to subscriptions

--- events/publisher.go
+++ events/publisher.go
@@ -6,37 +6,40 @@ import (
 )
 
 type Publisher interface {
-	Subscribe(name string, subscribeFunc func())
-	UnSubscribe(name string, subscribeFunc func())
+	Subscribe(name string, subscribeFunc *func())
+	UnSubscribe(name string, subscribeFunc *func())
 	Publish(name string)
 }
 
 type publisher struct {
-	subscriptions map[string]map[uintptr]func()
+	subscriptions map[string]map[uintptr]*func()
 	isBusy        chan bool
 }
 
 func NewPublisher() Publisher {
 	return &publisher{
-		subscriptions: make(map[string]map[uintptr]func()),
+		subscriptions: make(map[string]map[uintptr]*func()),
 		isBusy:        make(chan bool, 1),
 	}
 }
 
-func (this *publisher) Subscribe(name string, subscribeFunc func()) {
+func (this *publisher) Subscribe(name string, subscribeFunc *func()) {
 	errorhandler.CheckNilParameter(map[string]interface{}{"subscribeFunc": subscribeFunc})
 	this.isBusy <- true
+	if _, isContained := this.subscriptions[name]; !isContained {
+		this.subscriptions[name] = make(map[uintptr]*func())
+	}
 	pointer := reflect.ValueOf(subscribeFunc).Pointer()
 	if _, isContained := this.subscriptions[name][pointer]; !isContained {
-		this.subscriptions[name] = map[uintptr]func(){pointer: subscribeFunc}
+		this.subscriptions[name][pointer] = subscribeFunc
 	}
 	<-this.isBusy
 }
 
-func (this *publisher) UnSubscribe(name string, subscribeFunc func()) {
+func (this *publisher) UnSubscribe(name string, subscribeFunc *func()) {
 	errorhandler.CheckNilParameter(map[string]interface{}{"subscribeFunc": subscribeFunc})
 	this.isBusy <- true
-	subscriptions := make(map[uintptr]func())
+	subscriptions := make(map[uintptr]*func())
 	for pointer, s := range this.subscriptions[name] {
 		if pointer != reflect.ValueOf(subscribeFunc).Pointer() {
 			subscriptions[pointer] = s
@@ -51,6 +54,6 @@ func (this *publisher) Publish(name string) {
 	subsciptions := this.subscriptions[name]
 	<-this.isBusy
 	for _, f := range subsciptions {
-		errorhandler.OnErrorContinue(f)
+		errorhandler.OnErrorContinue(*f)
 	}
 }
