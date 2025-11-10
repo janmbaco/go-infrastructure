a51579f1b03ded68fb382af58495273dc9bafeaf
José Ángel Navarro <janmbaco@gmail.com>
2025-11-10 02:40:55 +0100

    linter

--- disk/fdlimit/fdlimit_unix.go
+++ disk/fdlimit/fdlimit_unix.go
@@ -3,12 +3,18 @@
 
 package fdlimit
 
-import "syscall"
+import (
+	"math"
+	"syscall"
+)
 
 func Get() (int, error) {
 	var limit syscall.Rlimit
 	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
 		return 0, err
+	}
+	if limit.Cur > math.MaxInt {
+		return math.MaxInt, nil
 	}
 	return int(limit.Cur), nil
 }
